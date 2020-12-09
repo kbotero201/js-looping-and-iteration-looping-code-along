26da57b07c5b26980d510ac18ef900a732206eed
kbotero201 <kbotero201@gmail.com>
2020-12-09 14:27:41 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    for (let i = 0; i < names.length; i++)
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+}
+
+//writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+
+
+function countDown(number) {
+    let countup = number;
+        while (countup > -1) {
+        console.log(countup--);
+    } 
+}
+
+
+//countDown(10); 
+
